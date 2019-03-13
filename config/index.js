142ed9108aa2493033cce2501a5d25d70b3308c6
Mehmet Arici <mehmetaricistd@gmail.com>
2019-03-13 20:29:57 +0300

    name refactored

--- config/index.js
+++ config/index.js
@@ -50,7 +50,6 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://crisgon.github.io/vue-cart/dist/',
 
     /**
      * Source Maps
